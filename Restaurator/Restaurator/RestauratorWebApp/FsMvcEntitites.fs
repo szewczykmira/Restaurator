d838670e6183c5023bb4b1fc8ada068fc5071122
Mira <szewczyk.mira@gmail.com>
2016-01-28 21:50:53 +0100

    Added project and first versions of models

---
+++ Restaurator/Restaurator/RestauratorWebApp/FsMvcEntitites.fs
@@ -0,0 +1,17 @@
+﻿namespace FsWeb.Repositories
+
+open System.Data.Entity
+open FsWeb.Models
+
+type MyContext() =
+    inherit DbContext("server=some;database=dbname;user id=uid;password=pwd")
+
+    [<DefaultValue>] val mutable users : DbSet<User>
+    member x.Users with get() = x.users and set v = x.users <- v
+
+    [<DefaultValue>] val mutable restaurants : DbSet<Restaurant>
+    member x.Restaurants with get() = x.restaurants and set v = x.restaurants <- v
+
+    [<DefaultValue>] val mutable opinions : DbSet<Opinion>
+    member x.Opinions with get() = x.opinions and set v = x.opinions <- v
+
---
+++ Restaurator/Restaurator/RestauratorWebApp/Models.fs
@@ -0,0 +1,97 @@
+﻿namespace FsWeb.Models
+
+open System
+open System.Collections.Generic
+open System.Data.Entity
+open System.ComponentModel.DataAnnotations
+
+type User () =
+    let mutable firstName = ""
+    let mutable email = ""
+    let mutable pswd = ""
+    let mutable res = Unchecked.defaultof<ICollection<Restaurant>>
+    let mutable opi = Unchecked.defaultof<ICollection<Opinion>>
+    
+    [<Key>]
+    member val Id = Guid.NewGuid() with get, set
+
+    [<Required>]
+    [<Display(Name = "First Name")>]
+    member x.FirstName with get() = firstName and set v = firstName <- v
+
+    [<Required>]
+    [<EmailAddress>]
+    [<DataType(DataType.EmailAddress)>]
+    [<Display(Name = "Email")>]
+    member x.Email with get() = email and set e = email <- e
+
+
+    [<Required>]
+    [<MinLength(10)>]
+    [<DataType(DataType.Password)>]
+    [<Display(Name = "Password")>]
+    member x.Password with get () = pswd and set p = pswd <- p
+
+    abstract member Restaurant : ICollection<Restaurant> with get, set
+    override x.Restaurant with get() = res and set v = res <- v
+
+    abstract member Opinion : ICollection<Opinion> with get, set
+    override x.Opinion with get() = opi and set v = opi <- v
+
+
+and Restaurant () =
+    let mutable name = ""
+    let mutable dscr = ""
+    let mutable adr = ""
+    let mutable uid = 0
+    let mutable user = Unchecked.defaultof<User>
+    let mutable opi = Unchecked.defaultof<ICollection<Opinion>>
+
+    [<Key>] 
+    member val Id = Guid.NewGuid() with get, set
+
+    [<Required>]
+    [<Display(Name = "Name")>]
+    member this.Name with get() = name and set v = name <- v
+
+    [<Required>]
+    [<Display(Name="Description")>]
+    member this.Description with get() = dscr and set v = dscr <- v
+
+    [<Required>]
+    [<Display(Name="Address")>]
+    member this.Address with get () = adr and set v = adr <- v
+
+    member x.User with get() = user and set v = user <- v
+    member x.UserId with get() = uid and set v = uid <- v
+
+    abstract member Opinion : ICollection<Opinion> with get, set
+    override x.Opinion with get() = opi and set v = opi <- v
+
+
+and Opinion () =
+    let mutable scr = 1
+    let mutable uid =  1
+    let mutable dscr = ""
+    let mutable uid = 0
+    let mutable user = Unchecked.defaultof<User>
+    let mutable rid = 0
+    let mutable rest = Unchecked.defaultof<Restaurant>
+
+    [<Key>] 
+    member val Id = Guid.NewGuid() with get, set
+
+    [<Required>]
+    [<Display(Name = "Score")>]
+    [<Range(1,6)>]
+    member this.Score with get () = scr and set v = scr <- v
+
+    [<Required>]
+    [<Display(Name="Description")>]
+    member this.Description with get() = dscr and set v = dscr <- v
+
+    member x.User with get() = user and set v = user <- v
+    member x.UserId with get() = uid and set v = uid <- v
+
+    member x.Restaurant with get() = rest and set v = rest <- v
+    member x.RestaurantId with get() = rid and set v = rid <- v
